7ce267a1a9ebc13b58ba8e4f6c8527d94a5d40cf
Brian Capouch <brianc@palaver.net>
2016-02-19 03:47:14 -0500

    Convert from optimist/prompt to yargs

--- sharpConvert.js
+++ sharpConvert.js
@@ -1,35 +1,28 @@
-
+#!/usr/bin/env node
 //  sharpConvert.js: Convert an image file into Deep Zoom format
 //     Brian Capouch 13 December 2015
-//     This version prompts for names of image file and tiles base
-//       if the user does not provide them via the CL
+//       Modified 19 February 2016: Convert from optimist/prompt to yargs
 //     Output: .dzi file and directory of tile images
 //
 
 // Set up functionality
 var sharp    = require('sharp'),
-    prompt   = require('prompt'),
-    optimist = require('optimist');
+    argv = require('yargs')
+      .usage('Usage $0 --file [file] --dzi [file]')
+      .demand(['file','dzi'])
+      .argv;
 
-// Set overrides from command line
-prompt.override = optimist.argv;
-
-// Get ready to talk to the user
-prompt.start();
+// Get input from user
+var infile = argv.file,
+    dziname = argv.dzi,
+    dzibase = dziname + '.dzi';
 
 // Get input from user (or use argv values provide via override)
-prompt.get(['filename', 'dziname'], function (err, result) {
-  if (err) { return onErr(err); }
-  console.log('Command-line input received:');
-  console.log('Filename: ' + result.filename);
-  console.log('Dziname: ' + result.dziname);
-  var infile  = result.filename,
-      dzibase = result.dziname + '.dzi';
 
 // Perform conversion
 sharp(infile).tile(256).toFile(dzibase, function(err, info) {
   });
-});
+//});
 
 function onErr(err) {
   console.log(err);
